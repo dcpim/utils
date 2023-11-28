81549209efa82e02e95d59d9d817d7541b648cf3
s24 <s24@erza.dendory.net>
2023-11-28 21:50:49 +0000

    fixing linting

--- dcpim.py
+++ dcpim.py
@@ -1,6 +1,6 @@
 """ DCPIM is a general purpose Python 3.x library that contains a lot of
-    commonly done operations inside of a single package. (C) 2018-2023
-    Patrick Lambert - http://dendory.net - Provided under the MIT License
+	commonly done operations inside of a single package. (C) 2018-2023
+	Patrick Lambert - http://dendory.net - Provided under the MIT License
 """
 
 __VERSION__ = "1.0"
--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
+""" Setup file fopr PyPi. """
 from setuptools import setup
 import dcpim
 
 setup(
   name = 'dcpim',
   packages = ['dcpim'],
-  version = dcpim.dcpim.__VERSION__,
+  version = dcpim.__VERSION__,
   description = 'This is a general purpose Python 3.x library used by DCPIM.',
   author = 'Patrick Lambert',
   license = 'MIT',
