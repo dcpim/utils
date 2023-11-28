0718b878e6665539962da2a90bd84ceebd7f6964
s24 <s24@erza.dendory.net>
2023-11-28 21:40:28 +0000

    fixing linting issues

--- dcpim.py
+++ dcpim.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 """ DCPIM is a general purpose Python 3.x library that contains a lot of
     commonly done operations inside of a single package. (C) 2018-2023
     Patrick Lambert - http://dendory.net - Provided under the MIT License
---
+++ test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 -c "import dcpim;help(dcpim)" > USAGE.txt
